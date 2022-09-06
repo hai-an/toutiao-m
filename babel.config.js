e39bfd54768a9cf880499478747971dd65eb4a0d
hai-an <1207783846@qq.com>
2022-09-06 18:38:10 +0800

    完成项目初始目录结构-搭建

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,9 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
+  // module: {
+  //   rules: [
+  //     {test: /\.js$/,exclude: /node_modules/,loader: "babel-loader"}
+  //   ]
+  // }
 }
